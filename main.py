a7d949ca7eb7fc522d37795c50f0d5e6df759aaa
Taihong Xiao <xiaotaihong@momenta.ai>
2018-07-05 23:13:21 +0800

    minor

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import torch
 import torchvision
 import os
 import argparse
-
+use_cuda = True
 
 def generator(dataloader):
     while True:
@@ -16,13 +16,16 @@ def generator(dataloader):
 resnet50 = torchvision.models.resnet50(pretrained=False)
 resnet50.load_state_dict(torch.load('./models/resnet50-19c8e357.pth'))
 
+kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+train_set = torchvision.datasets.MNIST('./datasets/mnist/', train=True, transform=None, download=False)
+test_set = torchvision.datasets.MNIST('./datasets/mnist/', train=False, transform=None, download=False)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=32, shuffle=True, **kwargs)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=32, shuffle=True, **kwargs)
+# data_generator = generator(dataloader)
 
-dataset = torchvision.datasets.MNIST('./datasets/mnist/', train=True, transform=None, download=True)
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
-data_generator = generator(dataloader)
-
-for i in range(1):
-    data = next(data_generator)
-    print(data)
+for batch_idx, (data, target) in enumerate(train_loader):
+    # data = next(data_generator)
+    if batch_idx < 1:
+        print(data)
 
 
